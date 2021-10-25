c40440c1eae9c97d37a13a820acdbe0f31a32033
Isabelle Thorpe <isabelle_thorpe@outlook.com>
2021-10-25 10:57:27 +0100

    Movies Added

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using asp_mvc_movies.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -12,5 +13,7 @@ namespace asp_mvc_movies.Data
             : base(options)
         {
         }
+
+        public DbSet<Movie> Movies { get; set; }
     }
 }
---
+++ Models/Movie.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace asp_mvc_movies.Models
+{
+    public enum Genres
+    {
+        Comedy, Drama, Horror, Romance
+    }
+
+    /// <summary>
+    /// Return the values of the Movie title, date, genre and
+    /// price and assigns the values to these variables
+    /// </summary>
+    public class Movie
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [StringLength(60), Required]
+        public string Title { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+
+        [Required]
+        public Genres Genre { get; set; } = Genres.Drama;
+
+        [Range(1.99, 20.00), Required]
+        public decimal Price { get; set; }
+    }
+}
